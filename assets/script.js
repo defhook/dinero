027bfe3fc71dc1f3c47b83231069d9189cb3b7d6
Your Name <mcpgco@gmail.com>
2022-03-12 09:43:41 -0700

    added to api being able to present data to the front page only need to figure out how to tailor it to the users needs

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var requestCurrentUrl = "https://api.exchangerate.host/convert?from=USD&to=EUR";
 var requestHistoricUrl = "https://api.exchangerate.host/2020-04-04";
 
+ var requestURL = 'https://api.exchangerate.host/symbols';
 // amount can be converted straight from the requestCurrentUrl with parameter "amount=1200"
 
 //FETCH FOR THE CURRENT CURRENCY RATE
@@ -17,6 +18,4 @@ fetch(requestHistoricUrl)
 .then(function(response){
     return response.json();
 })
-    .then(function(data){
-        console.log(data);
-    });
+    
