32204df5b0adcea057394712f09096ce3c9cb618
CarlBush <Carl.BushS13@gmail.com>
2022-03-13 19:35:42 -0700

    some clean up

--- assets/script.js
+++ assets/script.js
@@ -85,23 +85,3 @@ function convert(currency1, currency2, value) {
 }
 
 generateStorageHistory();
-
-// amount can be converted straight from the requestCurrentUrl with parameter "amount=1200"
-
-/*//FETCH FOR THE CURRENT CURRENCY RATE
-fetch(requestCurrentUrl)
-.then(function(response){
-    return response.json();
-})
-    .then(function(data){
-        console.log(data);
-    });*/
-
-//FETCH FOR THE HISTROICAL CURRENCY RATE
-/*fetch(requestHistoricUrl)
-.then(function(response){
-    return response.json();
-})
-    .then(function(data){
-        console.log(data);
-    })*/
